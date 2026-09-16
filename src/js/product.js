1bd3939dfbafb50360e1903e08471905e1e9addc
gershon-project <gershonn767@gmail.com>
2026-09-15 15:42:11 -1200

    Revert "Add dynamic header/footer loading and refactor cart with ShoppingCart class"
    This reverts commit c3c0ceda918ed6ee082de1746bcf56304606b20b.

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,6 @@
 import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
-
-loadHeaderFooter();
 
 const productId = getParam("product");
 const dataSource = new ProductData("tents");
