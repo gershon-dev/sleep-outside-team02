4706a1fdcad73827f51093d4b3c7fadfb2fa13ad
BYUSAIZE <prospersaize@gmail.com>
2026-09-25 18:28:15 +0200

    Fix baseURL ternary fallback

--- src/js/Externalservices.mjs
+++ src/js/Externalservices.mjs
@@ -1,4 +1,4 @@
-const baseURL = import.meta.env.VITE_SERVER_URL || 'https://wdd330-backend.onrender.com/';
+const baseURL = import.meta.env.VITE_SERVER_URL ? import.meta.env.VITE_SERVER_URL : 'https://wdd330-backend.onrender.com/';
 
 async function convertToJson(res) {
   const jsonResponse = await res.json();
